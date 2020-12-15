084c51a8fe995d57c0f827b50684ecbbc25612ac
Lucas Martins <lmlucasstone@gmail.com>
2020-12-15 09:50:51 -0300

    alteracoes na register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Form, Formik, Field } from 'formik';
 import * as Yup from 'yup';
-
 import { AuthLayout } from '../_layouts/auth';
 
 import './styles.css';
@@ -14,14 +13,16 @@ const initialValues = {
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string()
-    .min(3, 'O nome deve ter pelo menos 3 caracteres.')
-    .required('O nome é obrigatório.'),
+    .min(3, `*O nome deve ter pelo menos 3 caracteres.`)
+    .required('*O nome é obrigatório.'),
   lastName: Yup.string()
-    .min(2, 'O sobrenome deve ter pelo menos 3 caracteres.')
-    .required('O sobrenome é obrigatório.'),
+    .min(2, '*O sobrenome deve ter pelo menos 2 caracteres.')
+    .required('*O sobrenome é obrigatório.'),
   email: Yup.string()
-    .email('Formato de e-mail inválido.')
-    .required('O e-mail é obrigatório.'),
+    .email('*Formato de e-mail inválido.')
+    .required('*O e-mail é obrigatório.'),
+  password: Yup.string().required('*A senha é obrigatório.'),
+  date: Yup.string().required('*A data de nascimento é obrigatório.'),
 });
 
 const Register = () => {
@@ -46,21 +47,58 @@ const Register = () => {
             {({ errors, touched }) => (
               <Form>
                 <div className="input-block">
-                  <Field type="text" name="firstName" placeholder="Nome" />
-                  {touched.firstName && errors.firstName && (
-                    <span>{errors.firstName}</span>
-                  )}
+                  * <Field type="text" name="firstName" placeholder="Nome" />
+                  <div className="error-msg">
+                    {touched.firstName && errors.firstName && (
+                      <span>{errors.firstName}</span>
+                    )}
+                  </div>
+                </div>
+
+                <div className="input-block">
+                  *<Field type="text" name="lastName" placeholder="Sobrenome" />
+                  <div className="error-msg">
+                    {touched.lastName && errors.lastName && (
+                      <span>{errors.lastName}</span>
+                    )}
+                  </div>
                 </div>
                 <div className="input-block">
-                  <Field type="text" name="lastName" placeholder="Sobrenome" />
-                  {touched.lastName && errors.lastName && (
-                    <span>{errors.lastName}</span>
-                  )}
+                  * <Field type="email" name="email" placeholder="E-mail" />
+                  <div className="error-msg">
+                    {touched.email && errors.email && (
+                      <span>{errors.email}</span>
+                    )}
+                  </div>
                 </div>
                 <div className="input-block">
-                  <Field type="email" name="email" placeholder="E-mail" />
-                  {touched.email && errors.email && <span>{errors.email}</span>}
+                  *<Field type="password" name="password" placeholder="Senha" />
+                  <div className="error-msg">
+                    {touched.password && errors.password && (
+                      <span>{errors.password}</span>
+                    )}
+                  </div>
                 </div>
+
+                <h3>Data de Nascimento:</h3>
+                <div className="input-block">
+                  *<Field type="date" name="date" />
+                  <div className="input-block">
+                    <span>{errors.date}</span>
+                  </div>
+                </div>
+
+                <h3>Gênero:</h3>
+
+                <div className="input-block-radio">
+                  <div role="group" aria-labelledby="my-radio-group">
+                    <Field type="radio" name="sexo" value="masculino" />
+                    <span id="txtsexo">Masculino</span>
+                    <Field type="radio" name="sexo" value="feminino" />
+                    <span id="txtsexo">Feminino</span>
+                  </div>
+                </div>
+
                 <button type="submit">Cadastrar</button>
               </Form>
             )}
--- src/pages/_layouts/auth/index.js
+++ src/pages/_layouts/auth/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import logo from '../../../assets/images/logo.png';
+// import logo from '../../../assets/images/backInsideL.png';
 
 import './styles.css';
 
@@ -8,9 +8,7 @@ const AuthLayout = ({ children }) => {
   return (
     <div id="auth-wrapper">
       <aside>
-        <div className="logo-container">
-          <img src={logo} alt="Logo" className="logo" />
-        </div>
+        <div className="logo-container" />
       </aside>
       <main>{children}</main>
       <footer>
